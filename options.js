9475a6b46d3be54db6ea40e2c87411e0f8173ec0
Nick Orlando <nick+github@nickorlando.net>
2017-03-24 02:32:36 -0400

    update localStorage access and remove projects

--- options.js
+++ options.js
@@ -14,44 +14,39 @@ _gaq.push(['_trackPageview']);
 function save_options() {
 	var searchUrl = document.getElementById('searchUrl').value;
 	var suffix = document.getElementById('searchSuffix').value;
-	var projects = document.getElementById('jiraProjects').value;
 	console.log(searchUrl);
 	console.log(suffix);
   
-	localStorage['baseUrl'] = searchUrl;
-	localStorage['searchSuffix'] = suffix;
-	localStorage['jiraProjects'] = projects;
-	
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-		status.textContent = '';
-    }, 750);
+	// localStorage['baseUrl'] = searchUrl;
+	// localStorage['searchSuffix'] = suffix;
+	 chrome.storage.sync.set({'baseUrl': searchUrl, 'searchSuffix': suffix	}, function() {      
+		// Update status to let user know options were saved.
+		var status = document.getElementById('status');
+		status.textContent = 'Options saved.';
+		setTimeout(function() {
+			status.textContent = '';
+		}, 2000);
+    });
   
 }
 
 function restore_options() {
-	// Use default value
-	if (localStorage['baseUrl'] == undefined){
-		localStorage['baseUrl'] = 'http://my-jira-url/';
-	}
-	if(localStorage['searchSuffix'] == undefined){
-		localStorage['searchSuffix'] = 'secure/QuickSearch.jspa?searchString=';
-	}
-	if(localStorage['jiraProjects'] == undefined){
-		localStorage['jiraProjects'] = 'ABC, DEF';
-	}
+	 chrome.storage.sync.get(['baseUrl', 'searchSuffix','jiraProjects'], function(items) {
+		document.getElementById('searchUrl').value = items['baseUrl'] || 'http://my-jira-url/';
+		document.getElementById('searchSuffix').value = items['searchSuffix'] || 'secure/QuickSearch.jspa?searchString=';	
+		
+		// Update status to let user know options were saved.
+		var status = document.getElementById('status');
+		if (items['baseUrl']){
+			status.textContent = 'Options loaded successfully.';
+		}else{
+			status.textContent = 'A Jira server URL is required.';
+		}
+		setTimeout(function() {
+			status.textContent = '';
+		}, 2000);
+	});
 	
-	document.getElementById('searchUrl').value = localStorage['baseUrl'];
-	document.getElementById('searchSuffix').value = localStorage['searchSuffix'];
-	document.getElementById('jiraProjects').value = localStorage['jiraProjects'];
-	// Update status to let user know options were saved.
-	var status = document.getElementById('status');
-	status.textContent = 'Options loaded successfully.';
-	setTimeout(function() {
-		status.textContent = '';
-	}, 750);
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click', save_options);
